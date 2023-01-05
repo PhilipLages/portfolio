58e7acac95879665b65c421ace35aecf85331a12
Philip Lages <psilvaelages@gmail.com>
2023-01-05 13:52:03 -0300

    style: applies style to developer name and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
+
+import './styles/components/app.sass';
+
 function App() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <main id="portfolio">
+      <h1>Philip Lages</h1>
+      <Sidebar />
+      <MainContent />
+    </main>
   )
 };
 
