24a79ef1b72c03dded79d31746b457518622abf1
Philip Lages <psilvaelages@gmail.com>
2023-01-06 14:01:14 -0300

    style: changes colors of gradient

---
+++ src/components/InformatiobContainer.jsx
@@ -0,0 +1,11 @@
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from 'react-icons/ai';
+
+import '../styles/components/informatiobContainer.sass';
+
+const InformatiobContainer = () => {
+  return (
+    <div>InformatiobContainer</div>
+  )
+}
+
+export default InformatiobContainer
--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,21 +3,33 @@ import { FaLinkedinIn,  FaGithub, FaInstagram } from 'react-icons/fa';
 import '../styles/components/socialNetworks.sass';
 
 const socialNetworks = [
-  { name: "linkedIn", icon: <FaLinkedinIn /> },
-  { name: "github", icon: <FaGithub /> },
-  { name: "instagram", icon: <FaInstagram /> }
+  { 
+    name: "linkedIn",
+    url: "https://www.linkedin.com/in/philiplages/",
+    icon: <FaLinkedinIn /> 
+  },
+  { 
+    name: "github",
+    url: "https://github.com/PhilipLages",
+    icon: <FaGithub /> 
+  },
+  { 
+    name: "instagram",
+    url: "https://www.instagram.com/philiplages/",
+    icon: <FaInstagram /> 
+  }
 ];
 
 const SocialNetworks = () => {
   return (
     <section id='social-networks'>
-      { socialNetworks.map(({ name, icon }) => (
-        <a href="#" className='social-btn' id={ name } key={ name }>
+      { socialNetworks.map(({ name, url, icon }) => (
+        <a href={ url } target='_blank' className='social-btn' id={ name } key={ name }>
           { icon }
         </a>
       )) }
     </section>
   )
-}
+};
 
 export default SocialNetworks;
